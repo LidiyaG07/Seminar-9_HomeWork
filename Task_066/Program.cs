e6f828188041e8b821d187f8280b98a70ac0b69c
Ledeya Gankova <gankoval.a.0108@mail.ru>
2023-01-15 01:38:30 +0300

    Task k seminaru 9

---
+++ Task_066/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите значение M и N: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+SummNums(m, n, summ);
+
+
+void SummNums(int m, int n, int summ)
+{
+    if (m > n)
+    {
+        Console.WriteLine("Введите числа от меньшего к большему.");
+        return;
+    }
+
+    for (int i = m; i <= n; i++)
+    {
+        summ += i;
+    }
+    Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n} равна {summ}.");
+
+}
---
+++ Task_068/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.WriteLine("Введите число M и N: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+static int Function(int n, int m)
+{
+    while (n != 0)
+    {
+        if (m == 0)
+            m = 1;
+        else
+            m = Function(n, m - 1);
+        n--;
+    }
+    return m + 1;
+}
+
+Console.WriteLine($"Функция Аккермана А({m},{n}) = " + Function(m, n));
+
